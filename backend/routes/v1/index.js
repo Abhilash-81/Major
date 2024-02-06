32eefc5bfb0e20c0fd7d103971a0bcbba27e5ffc
Abhilash <jampaniabhilash81@gmail.com>
2024-02-06 09:05:20 +0530

    Added Comment model

--- backend/routes/v1/index.js
+++ backend/routes/v1/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
+
 const {
   createTweet,
   getAllTweets,
 } = require("../../controllers/tweetController");
 
+const { toggleLike } = require("../../controllers/likeController");
+
 router.post("/tweets", createTweet);
 router.get("/communities", getAllTweets);
+router.post("/likes", toggleLike);
 
 module.exports = router;
