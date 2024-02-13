22e7561adc4f35748bd13803f9d206e45c2b440f
Abhilash <jampaniabhilash81@gmail.com>
2024-02-13 10:40:23 +0530

    login,logout are working data is being stored in redux and able to get data from redux user slice

--- frontend/src/Screens/Logout.js
+++ frontend/src/Screens/Logout.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../utils/userSlice";
 
 const Logout = () => {
-  return <div>Logout Page</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(removeUser());
+    navigate("/");
+  };
+
+  return (
+    <div className="bg-gray-100 p-6 rounded-md shadow-md text-center">
+      <h3 className="text-lg font-medium text-gray-800 mb-4">
+        Are you sure you want to logout?
+      </h3>
+      <button
+        onClick={handleLogout}
+        className="bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-4 rounded-md"
+      >
+        Yes
+      </button>
+    </div>
+  );
 };
 
 export default Logout;
