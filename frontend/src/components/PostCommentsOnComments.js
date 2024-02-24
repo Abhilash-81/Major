ae81b39a8a23048b6dbb6598c95bc7dc85e5269e
Abhilash <jampaniabhilash81@gmail.com>
2024-02-24 20:29:19 +0530

    Remove unnecessary code for posting comments on comments.

--- frontend/src/components/PostCommentsOnComments.js
+++ frontend/src/components/PostCommentsOnComments.js
@@ -3,17 +3,8 @@ import Axios from "axios";
 
 const PostCommentsOnComments = (content, modelId) => {
   console.log(content, modelId);
-  const userId = useSelector((state) => state.user.userId);
-  useEffect(async () => {
-    const response = await Axios.post(
-      `http://localhost:3000/api/v1/comments?modelId=${modelId.id}&modelType=Comment`,
-      {
-        content: content.content,
-        userId,
-      }
-    );
-    console.log(response);
-  }, []);
+
+  useEffect(async () => {}, []);
 };
 
 export default PostCommentsOnComments;
